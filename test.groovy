30dce7a6c14fa651e66f4b7084ec1bfe05f21c61
Vladislav <43294333+xoathx@users.noreply.github.com>
2025-04-06 19:32:00 +0300

    123

---
+++ test.groovy
@@ -0,0 +1,46 @@
+pipeline {
+    agent any
+
+    environment {
+        ALLURE_RESULTS_DIRECTORY = 'allure-results'
+        ALLURE_REPORT_DIRECTORY = 'allure-report'
+        DISPLAY = ":99"  // Указываем виртуальный дисплей Xvfb
+    }
+
+    stages {
+        stage('Playwright UI Tests') {
+            agent any
+            steps {
+                script {
+                    def mvnHome = tool name: 'maven', type: 'maven'
+                    // Запуск Xvfb
+                    sh 'sudo Xvfb :99 -screen 0 1280x1024x24 &'
+
+                    // Устанавливаем зависимости для Playwright
+                    sh "sudo -E ${mvnHome}/bin/mvn exec:java -e -Dexec.mainClass=com.microsoft.playwright.CLI -Dexec.args=\"install-deps\""
+
+                    // Запуск тестов
+                    sh "${mvnHome}/bin/mvn clean test -Dtest=SampleUITest"
+
+                    // Проверка результатов
+                    sh 'ls -la allure-results'  // Проверка наличия тестов
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            // Выводим путь для диагностики
+            echo "ALLURE_RESULTS_DIRECTORY=${env.ALLURE_RESULTS_DIRECTORY}"
+
+            // Генерация отчёта Allure
+            allure([
+                    includeProperties: false,
+                    jdk: '',
+                    results: [[path: 'allure-results']],
+                    reportBuildPolicy: 'ALWAYS'
+            ])
+        }
+    }
+}
