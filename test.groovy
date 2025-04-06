74f566011d3feb30921d760c2adba92009efa9c4
Vladislav <43294333+xoathx@users.noreply.github.com>
2025-04-06 20:03:57 +0300

    123

--- test.groovy
+++ test.groovy
@@ -8,31 +8,37 @@ pipeline {
     }
 
     stages {
-        stage('Api Test'){
-            agent any
-            steps{
-                script{
-                    def mvnHome = tool name: 'maven', type: 'maven'
-                    sh "${mvnHome}/bin/mvn clean test -Dsurefire.excludes=SampleUITest.java"
+        stage('Parallel Tests') {
+            parallel {
+                stage('Api Test') {
+                    agent any
+                    steps {
+                        script {
+                            def mvnHome = tool name: 'maven', type: 'maven'
+                            sh "${mvnHome}/bin/mvn clean test -Dsurefire.excludes=SampleUITest.java"
+                        }
+                    }
                 }
-            }
-        }
-        stage('Playwright UI Tests') {
-            agent any
-            steps {
-                script {
-                    def mvnHome = tool name: 'maven', type: 'maven'
-                    // Запуск Xvfb
-                    sh 'sudo Xvfb :99 -screen 0 1280x1024x24 &'
 
-                    // Устанавливаем зависимости для Playwright
-                    sh "sudo -E ${mvnHome}/bin/mvn exec:java -e -Dexec.mainClass=com.microsoft.playwright.CLI -Dexec.args=\"install-deps\""
+                stage('Playwright UI Tests') {
+                    agent any
+                    steps {
+                        script {
+                            def mvnHome = tool name: 'maven', type: 'maven'
 
-                    // Запуск тестов
-                    sh "${mvnHome}/bin/mvn clean test -Dtest=SampleUITest"
+                            // Запуск Xvfb
+                            sh 'sudo Xvfb :99 -screen 0 1280x1024x24 &'
 
-                    // Проверка результатов
-                    sh 'ls -la allure-results'  // Проверка наличия тестов
+                            // Устанавливаем зависимости для Playwright
+                            sh "sudo -E ${mvnHome}/bin/mvn exec:java -e -Dexec.mainClass=com.microsoft.playwright.CLI -Dexec.args=\"install-deps\""
+
+                            // Запуск UI тестов
+                            sh "${mvnHome}/bin/mvn clean test -Dtest=SampleUITest"
+
+                            // Проверка результатов
+                            sh 'ls -la allure-results'  // Проверка наличия отчётов
+                        }
+                    }
                 }
             }
         }
@@ -40,10 +46,8 @@ pipeline {
 
     post {
         always {
-            // Выводим путь для диагностики
             echo "ALLURE_RESULTS_DIRECTORY=${env.ALLURE_RESULTS_DIRECTORY}"
 
-            // Генерация отчёта Allure
             allure([
                     includeProperties: false,
                     jdk: '',
